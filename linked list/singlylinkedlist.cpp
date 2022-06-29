43c8ec9ccb11f6fb643c3f267a6db45ef78519de
catalyst16812 <catalyst16812@gmail.com>
2022-06-29 18:09:50 +0530

    dsa

---
+++ linked list/singlylinkedlist.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int key;
+        int data;
+        Node* next;
+
+        Node()
+        {
+            key=0;
+            data=0;
+            next=NULL;
+        }
+
+        Node(int k,int d)
+        {
+            key = k;
+            data = d;
+        }
+};
+
+class SinglyLinkedList{
+    public:
+        Node* head;
+
+    SinglyLinkedList()
+    {
+        head = NULL;
+    }
+    SinglyLinkedList(Node *n)
+    {
+        head = n;
+    }
+
+    Node* nodeExist(int k)
+    {
+        Node* temp = NULL;
+        Node* ptr = head;
+
+        while(ptr!=NULL)
+        {
+            if(ptr->key==k)
+        }
+
+    }
+}
+int mian()
+{
+    return 0;
+}
